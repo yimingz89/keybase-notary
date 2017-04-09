f25bbaf02ff337eb4a1fda24ad6dfaf90a2127b7
John Kuszmaul <john.kuszmaul@gmail.com>
2017-04-08 23:18:54 -0400

    Changed code to be able to visit any of the necessary keybase webpages.

--- src/main/java/org/keybase/Extract_merkle_root.java
+++ src/main/java/org/keybase/Extract_merkle_root.java
@@ -4,25 +4,33 @@
 // Purpose: This program first receieves the javascript from the
 // keybase website, and then extracts the merkle root from the
 // javascript.
-// NOTE: Does not work.
 
-package org.keybase;
+//package org.keybase; // can't get it to compile with this
 
-import org.json.*;
+import java.net.*;
+//import org.json.*; // not downloaded yet
 import java.util.*;
 import java.io.*;
 
 public class Extract_merkle_root {
 
     public static void main(String[] args) {
-	
+	String merkleroot = "";
+	try {
+	    merkleroot = getRoot(10);
+	}
+	catch (Exception e) {
+	    System.out.println("Error getting Root");
+	    System.exit(1);
+	}
+	System.out.print(merkleroot);
     }
 
     // returns the nth merkle root right now it just returns the most
     // recent merkle root because I don't know where to access the
     // rest.
-    public static String getRoot(int num) {
-	URL keybase = new URL("https://keybase.io/_/api/1.0/merkle/root.json");
+    public static String getRoot(int num) throws Exception {
+	URL keybase = new URL("https://keybase.io/_/api/1.0/merkle/root.json?seqno=" + num);
         URLConnection yc = keybase.openConnection();
         BufferedReader in = new BufferedReader(new InputStreamReader(yc.getInputStream()));
 	StringBuffer input = new StringBuffer();
