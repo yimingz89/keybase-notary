7bdaa9959f8fd0716abe7267c4dae94c84bcae80
Yiming Zheng <yimingz89@gmail.com>
2017-06-10 16:34:22 -0400

    added sigs class

---
+++ src/main/java/org/keybase/Sigs.java
@@ -0,0 +1,19 @@
+package org.keybase;
+
+public class Sigs {
+	private String sig;
+	private String fingerprint;
+	
+	   public String getSig() {
+	        return sig;
+	    }
+	    public void setSig(String sig) {
+	        this.sig = sig;
+	    }
+	    public String getFingerprint() {
+	        return fingerprint;
+	    }
+	    public void setFingerprint(String fingerprint) {
+	        this.fingerprint = fingerprint;
+	    }
+}
