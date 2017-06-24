46574368a0dffef7a8cc57910e072dc4ece8bbeb
John Kuszmaul <john.kuszmaul@gmail.com>
2017-06-24 15:27:41 -0400

    Changed spacing back to 4.

--- src/main/java/org/keybase/JSONParser.java
+++ src/main/java/org/keybase/JSONParser.java
@@ -15,50 +15,50 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 
 public class JSONParser {
 
-	public static String getMerkleRoot(int num) throws Exception {
-		URL url = new URL("https://keybase.io/_/api/1.0/merkle/root.json?seqno=1");
-		if (num >= 1) {
-			url = new URL("https://keybase.io/_/api/1.0/merkle/root.json?seqno=" + num);
-		} else {
-			System.out.println("ERROR: Not a valid URL");
-			System.exit(1);
-		}
-
-		ObjectMapper objectMapper = new ObjectMapper();
-		try {
-			MerkleRoot root = objectMapper.readValue(url.openStream(), MerkleRoot.class);
-			return root.getHash();
-		} catch (IOException e) {
-			e.printStackTrace();
-			return "";	
-		}
+    public static String getMerkleRoot(int num) throws Exception {
+	URL url = new URL("https://keybase.io/_/api/1.0/merkle/root.json?seqno=1");
+	if (num >= 1) {
+	    url = new URL("https://keybase.io/_/api/1.0/merkle/root.json?seqno=" + num);
+	} else {
+	    System.out.println("ERROR: Not a valid URL");
+	    System.exit(1);
 	}
 
-	public static int getCurrentSeqno() {
-		URL url;
-		try {
-			url = new URL("https://keybase.io/_/api/1.0/merkle/root.json");
-		} catch (Exception e) {
-			System.err.println("ERROR: Unable to access keybase merkle roots");
-			e.printStackTrace();
-			System.exit(1);
-			return -1; // included to make compiler happy,
-			// but this line of code should never be reached.
-		}
-
-
-		ObjectMapper objectMapper = new ObjectMapper();
-		try {
-			MerkleRoot root = objectMapper.readValue(url.openStream(), MerkleRoot.class);
-			return root.getSeqno();
-		} catch (IOException e) {
-			System.err.println("ERROR: IO Exception unable to read Merkle root from JSON.");
-			e.printStackTrace();
-			System.exit(1);
-			return -1; // included to make compiler happy,
-			// but this line of code should never be reached.
-		}
-
+	ObjectMapper objectMapper = new ObjectMapper();
+	try {
+	    MerkleRoot root = objectMapper.readValue(url.openStream(), MerkleRoot.class);
+	    return root.getHash();
+	} catch (IOException e) {
+	    e.printStackTrace();
+	    return "";	
 	}
+    }
+
+    public static int getCurrentSeqno() {
+	URL url;
+	try {
+	    url = new URL("https://keybase.io/_/api/1.0/merkle/root.json");
+	} catch (Exception e) {
+	    System.err.println("ERROR: Unable to access keybase merkle roots");
+	    e.printStackTrace();
+	    System.exit(1);
+	    return -1; // included to make compiler happy,
+	    // but this line of code should never be reached.
+	}
+
+
+	ObjectMapper objectMapper = new ObjectMapper();
+	try {
+	    MerkleRoot root = objectMapper.readValue(url.openStream(), MerkleRoot.class);
+	    return root.getSeqno();
+	} catch (IOException e) {
+	    System.err.println("ERROR: IO Exception unable to read Merkle root from JSON.");
+	    e.printStackTrace();
+	    System.exit(1);
+	    return -1; // included to make compiler happy,
+	    // but this line of code should never be reached.
+	}
+
+    }
 
 }
--- src/main/java/org/keybase/NotaryApp.java
+++ src/main/java/org/keybase/NotaryApp.java
@@ -33,269 +33,269 @@ import java.util.Scanner;
 
 public class NotaryApp extends CatenaApp {
 
-	private static final Logger log = LoggerFactory.getLogger(ServerApp.class);
-	private static ECKey chainKey;
-	private static CatenaServer server;
-	private static Scanner scanner;
-	private static int secondsBetween = 1; // waits only 1 second.
+    private static final Logger log = LoggerFactory.getLogger(ServerApp.class);
+    private static ECKey chainKey;
+    private static CatenaServer server;
+    private static Scanner scanner;
+    private static int secondsBetween = 1; // waits only 1 second.
 
-	private static int seqno = 0;
-	private static File numberHashPairs = new File("issuedStatements.txt");
+    private static int seqno = 0;
+    private static File numberHashPairs = new File("issuedStatements.txt");
 
-	public static void main(String[] args) throws Exception {
-		//BriefLogFormatter.init();
-		//BriefLogFormatter.initVerbose();
-		//BriefLogFormatter.initWithSilentBitcoinJ();
+    public static void main(String[] args) throws Exception {
+	//BriefLogFormatter.init();
+	//BriefLogFormatter.initVerbose();
+	//BriefLogFormatter.initWithSilentBitcoinJ();
 
-		if (args.length < 2) {
-			// TODO: handle root-of-trust txid for new wallets with reused keys 
-			System.err.println("Usage: <chain-secret-key> mainnet|testnet|regtest [<datadir>] [<root-of-trust-txid>]");
-			return;
-		}
-
-		// Parse the Bitcoin network we're connecting to
-		parseBitcoinNetwork(args, 1, "-server");
-		if(args.length > 2) {
-			directory = args[2];
-		}
-
-		maybeParseDataDirectory();
-
-		// Read the chain's secret key from the file (we do this *after* we've set up the NetworkParameters)
-		String chainKeyStr = args[0];
-		try {
-			chainKey = DumpedPrivateKey.fromBase58(params, chainKeyStr).getKey(); 
-		} catch(Exception e) {
-			System.err.println(Utils.fmt("Error decoding Bitcoin secret key '{}': {}.\n", chainKeyStr, e.getMessage()));
-			System.err.println("Stack trace from exception: " + Throwables.getStackTraceAsString(e));
-			System.exit(1);
-			return;
-		}
-
-		// Server can tell if it needs the chainKey or not, by looking in its wallet
-		server = new org.catena.server.CatenaServer(params, new File(directory), chainKey, null);
-		svc = server;
-		//WalletAppKit server = new WalletAppKit(params, new File(directory), "-server");
-
-		connectAndStart(new Runnable() {
-			@Override
-			public void run() {
-				notarizeKeybase();
-			}
-		});
+	if (args.length < 2) {
+	    // TODO: handle root-of-trust txid for new wallets with reused keys 
+	    System.err.println("Usage: <chain-secret-key> mainnet|testnet|regtest [<datadir>] [<root-of-trust-txid>]");
+	    return;
 	}
 
-	/** 
-	 * Creates transactions from Keybase Merkle Roots and thus
-	 * 'notarizes' Keybase. Run from the run() function in main.
-	 */
-	private static void notarizeKeybase() {
-		scanner = new Scanner(System.in);
-		try {
-			while (true) {
-				if (!maybeIssueStatements())
-					System.out.println("No new Merkle Roots to issue");
-				waitTime();
-			}
-		} catch(Throwable e) {
-			System.err.println("Exception occurred: " + Throwables.getStackTraceAsString(e));
-		}
+	// Parse the Bitcoin network we're connecting to
+	parseBitcoinNetwork(args, 1, "-server");
+	if(args.length > 2) {
+	    directory = args[2];
 	}
 
-	/** Waits secondsBetween seconds. This is called inbetween
-	 * attempts to issue a new Merkle Root.
-	 */
-	private static void waitTime() {
-		try {
-			Thread.sleep(1000 * secondsBetween);
-		} catch (InterruptedException e) {	    
-			Thread.currentThread().interrupt();
-		}
+	maybeParseDataDirectory();
+
+	// Read the chain's secret key from the file (we do this *after* we've set up the NetworkParameters)
+	String chainKeyStr = args[0];
+	try {
+	    chainKey = DumpedPrivateKey.fromBase58(params, chainKeyStr).getKey(); 
+	} catch(Exception e) {
+	    System.err.println(Utils.fmt("Error decoding Bitcoin secret key '{}': {}.\n", chainKeyStr, e.getMessage()));
+	    System.err.println("Stack trace from exception: " + Throwables.getStackTraceAsString(e));
+	    System.exit(1);
+	    return;
 	}
 
-	private static void issueStatementHandler(int sequenceNumber) throws InsufficientMoneyException, IOException, InterruptedException {
-		// If there's no root-of-trust TXN, we gotta take care of that first
-		if(ext.hasRootOfTrustTxid() == false) {
-			issueRootOfTrustHandler();
-			//return;
+	// Server can tell if it needs the chainKey or not, by looking in its wallet
+	server = new org.catena.server.CatenaServer(params, new File(directory), chainKey, null);
+	svc = server;
+	//WalletAppKit server = new WalletAppKit(params, new File(directory), "-server");
+
+	connectAndStart(new Runnable() {
+		@Override
+		public void run() {
+		    notarizeKeybase();
+		}
+	    });
+    }
+
+    /** 
+     * Creates transactions from Keybase Merkle Roots and thus
+     * 'notarizes' Keybase. Run from the run() function in main.
+     */
+    private static void notarizeKeybase() {
+	scanner = new Scanner(System.in);
+	try {
+	    while (true) {
+		if (!maybeIssueStatements())
+		    System.out.println("No new Merkle Roots to issue");
+		waitTime();
+	    }
+	} catch(Throwable e) {
+	    System.err.println("Exception occurred: " + Throwables.getStackTraceAsString(e));
+	}
+    }
+
+    /** Waits secondsBetween seconds. This is called inbetween
+     * attempts to issue a new Merkle Root.
+     */
+    private static void waitTime() {
+	try {
+	    Thread.sleep(1000 * secondsBetween);
+	} catch (InterruptedException e) {	    
+	    Thread.currentThread().interrupt();
+	}
+    }
+
+    private static void issueStatementHandler(int sequenceNumber) throws InsufficientMoneyException, IOException, InterruptedException {
+	// If there's no root-of-trust TXN, we gotta take care of that first
+	if(ext.hasRootOfTrustTxid() == false) {
+	    issueRootOfTrustHandler();
+	    //return;
+	} else {
+	    log.debug("Already have root-of-trust txid '{}'", ext.getRootOfTrustTxid());
+	}
+
+	String stmt;
+	try {
+	    stmt = JSONParser.getMerkleRoot(sequenceNumber);
+	} catch (Exception e) {
+	    stmt = "";
+	    System.exit(1);
+	}
+	//        System.out.print("Please type in your next issued statement: ");
+	//        String stmt = scanner.nextLine();
+	//        if(stmt.trim().isEmpty()) {
+	//            System.out.println("Cancelled due to empty statement. Please try again but type something in.");
+	//            return;
+	//        }
+	//        System.out.println(stmt); // for debugging
+	issueStatement(stmt);
+    }    
+
+    private static boolean maybeIssueStatements() {
+	int currentSeqno = JSONParser.getCurrentSeqno();
+	//    	if (currentSeqno == -1) {
+	//	    System.err.println("ERROR: IO Exception while getting seqno");
+	//	    System.exit(1);
+	//    	}
+	boolean issuedStatement = false;
+	if (seqno < currentSeqno) {
+	    seqno = currentSeqno;
+	    try {
+		issueStatementHandler(seqno);
+		System.out.println("Issued sequence number: " + seqno);
+		issuedStatement = true;
+	    } catch (Exception e) {
+		System.err.println("Exception occured while issuing statements: " + Throwables.getStackTraceAsString(e));
+		System.exit(1);
+	    }
+	}
+	//    	while (seqno < currentSeqno) {
+	//	    issuedStatement = true;
+	//	    seqno++;
+	//	    try {
+	//		issueStatementHandler(seqno);
+	//		System.out.println("Issued sequence number: " + seqno);
+	//	    } catch (Exception e) {
+	//		System.err.println(/"//Exception occurred while issuing statements: " + Throwables.getStackTraceAsString(e));
+	//		System.exit(1);
+	//	    }
+	//    }
+	return issuedStatement;
+    }
+
+
+    private static void issueStatement(String stmt)
+	throws InsufficientMoneyException, IOException, InterruptedException {
+
+	String seq = String.format("%010d", seqno);
+	String tx = (stmt);
+
+	if (seq.length() != 10) {
+	    System.out.println("ERROR: String length should be of size 10, but is of size " + seq.length());
+	    System.exit(1);;
+	}
+	if (tx.length() != 128)	{
+	    System.out.println("ERROR: Stmt length should be of size 128, but is of size " + stmt.length());
+	    System.exit(1);
+	}
+
+	try {
+	    Files.write(Paths.get("issuedStatements.txt"), (seq + " ").getBytes(), java.nio.file.StandardOpenOption.APPEND);
+	    Files.write(Paths.get("issuedStatements.txt"), (stmt + "\n").getBytes(), java.nio.file.StandardOpenOption.APPEND);
+	}catch (IOException e) {
+	    System.out.println("ERROR writing to file issuedStatmenets.txt: " + Throwables.getStackTraceAsString(e));
+	    System.exit(1);
+	}
+
+	Transaction txn = appendStatement(hexStringToByteArray(stmt));		
+
+	Transaction prevTx = CatenaUtils.getPrevCatenaTx(wallet, txn.getHash());
+
+	System.out.printf("Created tx '%s' with statement '%s' (prev tx is '%s')\n", txn.getHash(), stmt, prevTx.getHash());
+    }
+
+    public static byte[] hexStringToByteArray(String s) {
+	int len = s.length();
+	if (len % 2 == 1)
+	    System.err.println("String of odd lenth cannot be hex.");
+	byte[] data = new byte[len / 2];
+	for (int i = 0; i < len; i += 2) {
+	    data[i / 2] = (byte) ((Character.digit(s.charAt(i), 16) << 4)
+				  + Character.digit(s.charAt(i+1), 16));
+	}
+	return data;
+    }
+
+    private static Transaction appendStatement(byte[] stmt) throws InsufficientMoneyException, IOException, 
+								   InterruptedException {
+	Transaction txn = server.appendStatement(stmt);
+	if(isRegtestEnv) {
+	    // We generate the block ourselves if we are in regtest mode
+	    CatenaUtils.generateBlockRegtest();
+	}
+	return txn;
+    }
+
+
+    private static void issueRootOfTrustHandler() throws InsufficientMoneyException, IOException, InterruptedException {
+	Address chainAddr = wallet.getChainAddress();
+	System.out.printf("It looks like there is no Catena chain associated with the %s address from your wallet\n",
+			  chainAddr);
+
+	System.out.printf("If you'd like to start a new Catena chain for the %s address, please type in its name: ", chainAddr);
+	String chainName = scanner.nextLine();
+
+	issueRootOfTrust(chainName);
+    }
+
+    private static void issueRootOfTrust(String chainName) throws InsufficientMoneyException, IOException, 
+								  InterruptedException {
+	Transaction txn = appendStatement(chainName.getBytes());
+
+	System.out.printf("Created root-of-trust tx '%s' for chain '%s'\n", txn.getHash(), chainName);
+    }
+
+    private static void printPrivKeyHandler() {
+	printPrivKey();
+    }
+
+    private static void printPrivKey() {
+	System.out.println("Catena server's chain private key: " + wallet.getChainKey().getPrivateKeyAsWiF(params));
+    }
+
+    private static void genClientConfigFileHandler() throws IOException {
+	Path path;
+	while(true) {
+	    try {
+		System.out.print("Please enter the directory where you'd like the config.ini file to be saved (leave blank for 'catena-client/'): ");
+		String dirStr = scanner.nextLine();
+
+		if(dirStr.trim().isEmpty()) {
+		    dirStr = "catena-client";
+		}
+		Path dir = Paths.get(dirStr);
+		path = Paths.get(dir.toString(), "config.ini");
+
+		if(path.toFile().exists() == false) {
+		    if(dir.toFile().isFile()) {
+			System.out.printf("The %s directory you entered is actually a file, not a directory. Try again.\n", dir);
+			continue;
+		    }
+
+		    if(dir.toFile().exists() == false)
+			Files.createDirectory(dir);
+
+		    break;
 		} else {
-			log.debug("Already have root-of-trust txid '{}'", ext.getRootOfTrustTxid());
+		    System.out.printf("ERROR: We won't overwrite the already existing file at %s. Please pick a " + 
+				      "different directory where the config.ini file doesn't exist.\n", path);
 		}
-
-		String stmt;
-		try {
-			stmt = JSONParser.getMerkleRoot(sequenceNumber);
-		} catch (Exception e) {
-			stmt = "";
-			System.exit(1);
-		}
-		//        System.out.print("Please type in your next issued statement: ");
-		//        String stmt = scanner.nextLine();
-		//        if(stmt.trim().isEmpty()) {
-		//            System.out.println("Cancelled due to empty statement. Please try again but type something in.");
-		//            return;
-		//        }
-		//        System.out.println(stmt); // for debugging
-		issueStatement(stmt);
-	}    
-
-	private static boolean maybeIssueStatements() {
-		int currentSeqno = JSONParser.getCurrentSeqno();
-		//    	if (currentSeqno == -1) {
-		//	    System.err.println("ERROR: IO Exception while getting seqno");
-		//	    System.exit(1);
-		//    	}
-		boolean issuedStatement = false;
-		if (seqno < currentSeqno) {
-			seqno = currentSeqno;
-			try {
-				issueStatementHandler(seqno);
-				System.out.println("Issued sequence number: " + seqno);
-				issuedStatement = true;
-			} catch (Exception e) {
-				System.err.println("Exception occured while issuing statements: " + Throwables.getStackTraceAsString(e));
-				System.exit(1);
-			}
-		}
-		//    	while (seqno < currentSeqno) {
-		//	    issuedStatement = true;
-		//	    seqno++;
-		//	    try {
-		//		issueStatementHandler(seqno);
-		//		System.out.println("Issued sequence number: " + seqno);
-		//	    } catch (Exception e) {
-		//		System.err.println(/"//Exception occurred while issuing statements: " + Throwables.getStackTraceAsString(e));
-		//		System.exit(1);
-		//	    }
-		//    }
-		return issuedStatement;
+	    } catch(InvalidPathException e) {
+		System.out.println("\nERROR: You must enter a valid path! Try again...");
+	    }
 	}
 
+	genClientConfigFile(path);
+    }
 
-	private static void issueStatement(String stmt)
-			throws InsufficientMoneyException, IOException, InterruptedException {
-
-		String seq = String.format("%010d", seqno);
-		String tx = (stmt);
-
-		if (seq.length() != 10) {
-			System.out.println("ERROR: String length should be of size 10, but is of size " + seq.length());
-			System.exit(1);;
-		}
-		if (tx.length() != 128)	{
-			System.out.println("ERROR: Stmt length should be of size 128, but is of size " + stmt.length());
-			System.exit(1);
-		}
-
-		try {
-			Files.write(Paths.get("issuedStatements.txt"), (seq + " ").getBytes(), java.nio.file.StandardOpenOption.APPEND);
-			Files.write(Paths.get("issuedStatements.txt"), (stmt + "\n").getBytes(), java.nio.file.StandardOpenOption.APPEND);
-		}catch (IOException e) {
-			System.out.println("ERROR writing to file issuedStatmenets.txt: " + Throwables.getStackTraceAsString(e));
-			System.exit(1);
-		}
-
-		Transaction txn = appendStatement(hexStringToByteArray(stmt));		
-
-		Transaction prevTx = CatenaUtils.getPrevCatenaTx(wallet, txn.getHash());
-
-		System.out.printf("Created tx '%s' with statement '%s' (prev tx is '%s')\n", txn.getHash(), stmt, prevTx.getHash());
-	}
-
-	public static byte[] hexStringToByteArray(String s) {
-		int len = s.length();
-		if (len % 2 == 1)
-			System.err.println("String of odd lenth cannot be hex.");
-		byte[] data = new byte[len / 2];
-		for (int i = 0; i < len; i += 2) {
-			data[i / 2] = (byte) ((Character.digit(s.charAt(i), 16) << 4)
-					+ Character.digit(s.charAt(i+1), 16));
-		}
-		return data;
-	}
-
-	private static Transaction appendStatement(byte[] stmt) throws InsufficientMoneyException, IOException, 
-	InterruptedException {
-		Transaction txn = server.appendStatement(stmt);
-		if(isRegtestEnv) {
-			// We generate the block ourselves if we are in regtest mode
-			CatenaUtils.generateBlockRegtest();
-		}
-		return txn;
-	}
-
-
-	private static void issueRootOfTrustHandler() throws InsufficientMoneyException, IOException, InterruptedException {
-		Address chainAddr = wallet.getChainAddress();
-		System.out.printf("It looks like there is no Catena chain associated with the %s address from your wallet\n",
-				chainAddr);
-
-		System.out.printf("If you'd like to start a new Catena chain for the %s address, please type in its name: ", chainAddr);
-		String chainName = scanner.nextLine();
-
-		issueRootOfTrust(chainName);
-	}
-
-	private static void issueRootOfTrust(String chainName) throws InsufficientMoneyException, IOException, 
-	InterruptedException {
-		Transaction txn = appendStatement(chainName.getBytes());
-
-		System.out.printf("Created root-of-trust tx '%s' for chain '%s'\n", txn.getHash(), chainName);
-	}
-
-	private static void printPrivKeyHandler() {
-		printPrivKey();
-	}
-
-	private static void printPrivKey() {
-		System.out.println("Catena server's chain private key: " + wallet.getChainKey().getPrivateKeyAsWiF(params));
-	}
-
-	private static void genClientConfigFileHandler() throws IOException {
-		Path path;
-		while(true) {
-			try {
-				System.out.print("Please enter the directory where you'd like the config.ini file to be saved (leave blank for 'catena-client/'): ");
-				String dirStr = scanner.nextLine();
-
-				if(dirStr.trim().isEmpty()) {
-					dirStr = "catena-client";
-				}
-				Path dir = Paths.get(dirStr);
-				path = Paths.get(dir.toString(), "config.ini");
-
-				if(path.toFile().exists() == false) {
-					if(dir.toFile().isFile()) {
-						System.out.printf("The %s directory you entered is actually a file, not a directory. Try again.\n", dir);
-						continue;
-					}
-
-					if(dir.toFile().exists() == false)
-						Files.createDirectory(dir);
-
-					break;
-				} else {
-					System.out.printf("ERROR: We won't overwrite the already existing file at %s. Please pick a " + 
-							"different directory where the config.ini file doesn't exist.\n", path);
-				}
-			} catch(InvalidPathException e) {
-				System.out.println("\nERROR: You must enter a valid path! Try again...");
-			}
-		}
-
-		genClientConfigFile(path);
-	}
-
-	private static void genClientConfigFile(Path path) throws IOException {
-		if(ext.hasRootOfTrustTxid()) {
-			FileWriter writer = new FileWriter(path.toFile());
-			writer.write(Utils.fmt("pubkey={}\n", wallet.getChainAddress()));
-			writer.write(Utils.fmt("txid={}\n", ext.getRootOfTrustTxid()));
-			writer.write(Utils.fmt("btc_env={}\n", btcnet)); 
-			writer.close();
-			System.out.printf("\nCatena client's %s file was written successfully!\n", path);
-		} else {
-			System.out.println("\nERROR: The root-of-trust TXN hasn't been created yet. Please issue your first statement!");
-		}        
-	}
+    private static void genClientConfigFile(Path path) throws IOException {
+	if(ext.hasRootOfTrustTxid()) {
+	    FileWriter writer = new FileWriter(path.toFile());
+	    writer.write(Utils.fmt("pubkey={}\n", wallet.getChainAddress()));
+	    writer.write(Utils.fmt("txid={}\n", ext.getRootOfTrustTxid()));
+	    writer.write(Utils.fmt("btc_env={}\n", btcnet)); 
+	    writer.close();
+	    System.out.printf("\nCatena client's %s file was written successfully!\n", path);
+	} else {
+	    System.out.println("\nERROR: The root-of-trust TXN hasn't been created yet. Please issue your first statement!");
+	}        
+    }
 }
