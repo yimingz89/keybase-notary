94eb189ccb4ee13430d5dd161523b0e84a25971d
yiming <yiming@ubuntu1>
2017-06-25 22:27:42 -0400

    done testing

--- src/main/java/org/keybase/NotaryApp.java
+++ src/main/java/org/keybase/NotaryApp.java
@@ -30,7 +30,6 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.Scanner;
 
-// testing
 public class NotaryApp extends CatenaApp {
     
     private static final Logger log = LoggerFactory.getLogger(ServerApp.class);
